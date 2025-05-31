784ecd5ba7b1250f3abaab7bc4a953ff041545dd
rysk(mac) <swpa0x33d@gmail.com>
2025-05-31 22:34:55 +0900

    #2 feat:login

--- backend/app/Http/Controllers/Api/Auth/AuthController.php
+++ backend/app/Http/Controllers/Api/Auth/AuthController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Api\Auth;
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Resources\Auth\RegisterResource;
+use App\Http\Resources\Auth\LoginResource;
 use App\Services\AuthService;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -25,5 +28,21 @@ class AuthController extends Controller
         return (new RegisterResource($user))
             ->response()
             ->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        if (!Auth::attempt($request->all())) {
+            return response()->json([
+                'message' => 'Invalid credentials.',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('AccessToken')->plainTextToken;
+
+        return (new LoginResource($user, $token))
+            ->response()
+            ->setStatusCode(Response::HTTP_OK);
     }
 }
