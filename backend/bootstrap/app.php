75d9d8c0fd8ec52aeed3bb72abc56575d7427c52
rysk(mac) <swpa0x33d@gmail.com>
2025-06-02 19:14:16 +0900

    #1 feat: get authenticated user

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            return response()->json([
+                'message' => 'Unauthenticated.',
+            ], Response::HTTP_UNAUTHORIZED);
+        });
+
         $exceptions->render(function (Throwable $e, Request $request) {
             if ($request->expectsJson()) {
                 return response()->json([
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,3 +5,7 @@ use App\Http\Controllers\Api\Auth\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'getAuthenticatedUser']);
+});
