6ed3c55f7c57adbb848001732fa2b29a6faad8f6
rysk(mac) <swpa0x33d@gmail.com>
2025-06-02 20:03:10 +0900

    #2 refactor

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -18,6 +19,14 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'message' => 'Not Found.',
+                ], Response::HTTP_NOT_FOUND);
+            }
+        });
+
         $exceptions->render(function (AuthenticationException $e, Request $request) {
             return response()->json([
                 'message' => 'Unauthenticated.',
